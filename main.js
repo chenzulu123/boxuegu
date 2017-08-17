42b8e094edc0399ec69bcf1b0c3ad1a98e28e5a8
chenzulu123 <chenzulu@163.com>
2017-08-17 15:49:59 +0800

    新增博学谷主页布局

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-/**
- * 这里是文档注释
- * Author:Wilbert
- *   Date:2017/8/17
- */
+require.config({
+    baseUrl: "js",
+    paths: {
+        jquery: "./lib/jquery-2.1.4",
+        bootstrap: '../assets/bootstrap/js/bootstrap'
+    },
+    //这里的设置是为了让bootstrap等待jQuery加载完成之后才使用jQuery模块
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+        }
+    },
+});
+//引入模块
+require(["jquery","bootstrap"], function ($) {
+    //这段话表示开启严格模式
+    'use strict'
+    
+});
